154f2e64b30b686323b86d690aa42c0c6c28307b
Iwan Kata <iwankata01@gmail.com>
2021-03-10 13:05:28 +0000

    Zmiany do CMakeLists

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
-int main() {
+struct Field
+{
+  bool hasMine;
+  bool hasFlag;
+  bool isRevealed;
+};
+
+class MinesweeperBoard
+{
+  Field Board[100][100];
+  int width;
+  int height;
+
+public:
+  MinesweeperBoard();
+  void debug_display() const;
+};
+
+MinesweeperBoard::MinesweeperBoard()
+{
+  width = 10;
+  height = 10; // dziekuje
+
+  
+}
+
+
+int main() 
+{
   std::cout << "Hello World!\n";
 
   //Zmiana
