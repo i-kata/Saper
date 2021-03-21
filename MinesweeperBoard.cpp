be6994ffb3b7e6fa1fd5098938d5aa55ef59c7aa
Iwan Kata <iwankata01@gmail.com>
2021-03-21 23:31:43 +0000

    Drugi etap: - brak funckji getMineCount -brak funkcji getGameState -niedokończona funckja revealField

--- MinesweeperBoard.cpp
+++ MinesweeperBoard.cpp
@@ -1,6 +1,187 @@
 #include <iostream>
 #include "MinesweeperBoard.h"
 
+MinesweeperBoard::MinesweeperBoard(int width, int height, GameMode mode) : width(width), height(height)
+{
+    width = width;
+    height = height;
+    mode = mode;
+
+
+    for (int wier = 0; wier < height; wier++)
+    {
+        for (int kol = 0; kol < width; kol++)
+        {
+            if (wier == kol)
+                Board[wier][kol].hasMine = true;
+
+            if (wier == 0)
+                Board[wier][kol].hasMine = true;
+
+            if (kol == 0 and wier % 2 == 0)
+                Board[wier][kol].hasMine = true;
+
+            Board[wier][kol].isRevealed = true;
+            Board[wier][kol].hasFlag = false;
+        }
+    }
+}
+
+void MinesweeperBoard::debug_display() const
+{
+    for (int wier = 0; wier < height; wier++)
+    {
+        for (int kol = 0; kol < width; kol++)
+        {
+            if (Board[wier][kol].hasMine == true)
+                std::cout << "[M";
+            else
+                std::cout << "[.";
+
+            if (Board[wier][kol].isRevealed == true)
+                std::cout << "o";
+            else
+                std::cout << ".";
+
+            if (Board[wier][kol].hasFlag == true)
+                std::cout << "f]";
+            else
+                std::cout << ".]";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int MinesweeperBoard::getBoardWidth() const
+{
+    return width;
+}
+
+int MinesweeperBoard::getBoardHeight() const
+{
+    return height;
+}
+
+int MinesweeperBoard::countMines(int wier, int kol) const
+{
+    int minecount = 0;
+
+    if (Board[wier][kol].isRevealed == false)
+        return -1;
+
+    if (wier > height or kol > width)
+        return -1;
+
+    if (Board[wier - 1][kol - 1].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier][kol - 1].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier + 1][kol - 1].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier + 1][kol].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier - 1][kol].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier + 1][kol + 1].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier][kol + 1].hasMine == true)
+        minecount += 1;
+
+    if (Board[wier - 1][kol + 1].hasMine == true)
+        minecount += 1;
+
+    return minecount;
+}
+
+bool MinesweeperBoard::hasFlag(int wier, int kol) const
+{
+    if (wier > height or kol > width)
+        return false;
+
+    if (Board[wier][kol].hasFlag == false)
+        return false;
+
+    if (Board[wier][kol].isRevealed == true)
+        return false;
+
+    if (Board[wier][kol].hasFlag == true)
+        return true;
+}
+
+void MinesweeperBoard::toggleFlag(int wier, int kol)
+{
+    if (Board[wier][kol].isRevealed == false)
+        Board[wier][kol].hasFlag = true;
+
+    if (Board[wier][kol].isRevealed == true)
+        return;
+
+    if (wier > height or kol > width)
+        return;
+
+    if (FINISHED_LOSS or FINISHED_WIN)
+        return;
+}
+
+void MinesweeperBoard::revealField(int wier, int kol)
+{
+    if (Board[wier][kol].isRevealed == true)
+        return;
+
+    if (wier > height or kol > width)
+        return;
+
+    if (FINISHED_LOSS or FINISHED_WIN)
+        return;
+
+    if (Board[wier][kol].hasFlag == true)
+        return;
+
+    if (Board[wier][kol].isRevealed == false and Board[wier][kol].hasMine == false)
+        Board[wier][kol].isRevealed = true;
+
+    if (Board[wier][kol].isRevealed == false and Board[wier][kol].hasMine == true)
+    {
+        Board[wier][kol].isRevealed = true;
+        FINISHED_LOSS;
+    }
+
+}
+
+bool MinesweeperBoard::isRevealed(int wier, int kol) const
+{
+    if (Board[wier][kol].isRevealed == true)
+        return true;
+}
+
+char MinesweeperBoard::getFieldInfo(int wier, int kol) const
+{
+    if (wier > height or kol > width)
+        return '#';
+
+    if (Board[wier][kol].isRevealed == false and Board[wier][kol].hasFlag == true)
+        return 'F';
+
+    if (Board[wier][kol].isRevealed == false and Board[wier][kol].hasFlag == false)
+        return '_';
+
+    if (Board[wier][kol].isRevealed == true and Board[wier][kol].hasMine == true)
+        return 'x';
+
+    if (Board[wier][kol].isRevealed == true and countMines(wier, kol) == 0)
+        return ' ';
+
+    if (Board[wier][kol].isRevealed == true and countMines(wier, kol) != 0)
+        return countMines(wier, kol);
+}
+
+/*
 
 MinesweeperBoard::MinesweeperBoard(int width, int height, GameMode mode)
 {
@@ -17,7 +198,7 @@ MinesweeperBoard::MinesweeperBoard(int width, int height, GameMode mode)
     }
   }
 
-/*
+
   Board [0][0].hasMine = true;
   Board [0][0].hasFlag = false;
   Board [0][0].isRevealed = false;
@@ -29,7 +210,7 @@ MinesweeperBoard::MinesweeperBoard(int width, int height, GameMode mode)
   Board [0][2].hasMine = true;
   Board [0][2].hasFlag = true;
   Board [0][2].isRevealed = false;
-*/
+
 }
 
 void MinesweeperBoard::debug_display() const
@@ -56,3 +237,5 @@ void MinesweeperBoard::debug_display() const
     std::cout << std::endl;
   }
 }
+
+*/
--- MinesweeperBoard.h
+++ MinesweeperBoard.h
@@ -1,6 +1,45 @@
+#ifndef MinesweeperBoard_H_
+#define MinesweeperBoar_H_
 
-#ifndef Minesweeperboard_H_
-#define Minesweeperboard_H_
+enum GameMode { DEBUG, EASY, NORMAL, HARD} ;
+enum GameState { RUNNING, FINISHED_WIN, FINISHED_LOSS };
+
+struct Field
+{
+    bool hasMine;
+    bool hasFlag;
+    bool isRevealed;
+};
+
+class MinesweeperBoard
+{
+    Field Board[100][100];
+    int width;
+    int height;
+    
+    
+
+public:
+    static int minecount;
+
+    MinesweeperBoard(int width, int height, GameMode mode);
+    void debug_display() const;
+    int getBoardWidth() const;
+    int getBoardHeight() const;
+    int getMineCount() const;
+    int countMines(int wier, int kol) const;
+    bool hasFlag(int wier, int kol);
+    void toggleFlag(int wier, int kol);
+    void revealField(int wier, int kol) const;
+    bool isRevealed(int wier, int kol) const;
+    GameState getGameState() const;
+    char getFieldInfo(int row, int col) const;
+};
+
+#endif
+
+
+/*
 
 enum GameMode { DEBUG, EASY, NORMAL, HARD };
 enum GameState { RUNNING, FINISHED_WIN, FINISHED_LOSS };
@@ -23,5 +62,4 @@ public:
   void debug_display() const;
 };
 
-
-#endif
+*/
