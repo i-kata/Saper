07676f5a49b1fc5676b60da8cc622a35be2caeae
Iwan Kata <iwankata01@gmail.com>
2021-03-23 01:16:16 +0000

    Drugi etap skończony

--- MinesweeperBoard.cpp
+++ MinesweeperBoard.cpp
@@ -7,25 +7,67 @@ MinesweeperBoard::MinesweeperBoard(int width, int height, GameMode mode) : width
     height = height;
     mode = mode;
 
+    int minecount = 0;
 
     for (int wier = 0; wier < height; wier++)
     {
-        for (int kol = 0; kol < width; kol++)
+
+        if (mode == DEBUG)
         {
-            if (wier == kol)
-                Board[wier][kol].hasMine = true;
+            for (int kol = 0; kol < width; kol++)
+            {
+                if (wier == kol)
+                    Board[wier][kol].hasMine = true;
 
-            if (wier == 0)
-                Board[wier][kol].hasMine = true;
+                if (wier == 0)
+                    Board[wier][kol].hasMine = true;
 
-            if (kol == 0 and wier % 2 == 0)
-                Board[wier][kol].hasMine = true;
+                if (kol == 0 and wier % 2 == 0)
+                    Board[wier][kol].hasMine = true;
 
-            Board[wier][kol].isRevealed = true;
-            Board[wier][kol].hasFlag = false;
+                Board[wier][kol].isRevealed = true;
+                Board[wier][kol].hasFlag = false;
+            }
         }
+
+        if (mode == EASY)
+        {
+            int mines = 0.1 * width * height;
+            
+            while (minecount != mines + 1)
+            {
+                Board[rand() % height][rand() % width].hasMine = true;
+                minecount++;
+            }
+        }
+
+        if (mode == NORMAL)
+        {
+            int mines = 0.2 * width * height;
+
+            while (minecount != mines + 1)
+            {
+                Board[rand() % height][rand() % width].hasMine = true;
+                minecount++;
+            }
+        }
+
+        if (mode == HARD)
+        {
+            int mines = 0.3 * width * height;
+
+            while (minecount != mines + 1)
+            {
+                Board[rand() % height][rand() % width].hasMine = true;
+                minecount++;
+            }
+
+        }
+
     }
+
 }
+
 
 void MinesweeperBoard::debug_display() const
 {
@@ -48,19 +90,41 @@ void MinesweeperBoard::debug_display() const
             else
                 std::cout << ".]";
         }
+
         std::cout << std::endl;
     }
+
 }
+
 
 int MinesweeperBoard::getBoardWidth() const
 {
     return width;
 }
 
+
 int MinesweeperBoard::getBoardHeight() const
 {
     return height;
 }
+
+
+int MinesweeperBoard::getMineCount() const
+{
+    int minecount = 0;
+
+    for (int wier = 0; wier < height; wier++)
+    {
+        for (int kol = 0; kol < width; kol++)
+        {
+            if (Board[wier][kol].hasMine == true)
+                minecount++;
+        }
+    }
+
+    return minecount;
+}
+
 
 int MinesweeperBoard::countMines(int wier, int kol) const
 {
@@ -99,6 +163,7 @@ int MinesweeperBoard::countMines(int wier, int kol) const
     return minecount;
 }
 
+
 bool MinesweeperBoard::hasFlag(int wier, int kol) const
 {
     if (wier > height or kol > width)
@@ -113,6 +178,7 @@ bool MinesweeperBoard::hasFlag(int wier, int kol) const
     if (Board[wier][kol].hasFlag == true)
         return true;
 }
+
 
 void MinesweeperBoard::toggleFlag(int wier, int kol)
 {
@@ -129,8 +195,18 @@ void MinesweeperBoard::toggleFlag(int wier, int kol)
         return;
 }
 
+
+bool MinesweeperBoard::isRevealed(int wier, int kol) const
+{
+    if (Board[wier][kol].isRevealed == true)
+        return true;
+}
+
+
 void MinesweeperBoard::revealField(int wier, int kol)
 {
+    bool notTouchedBoard = true;
+
     if (Board[wier][kol].isRevealed == true)
         return;
 
@@ -148,17 +224,35 @@ void MinesweeperBoard::revealField(int wier, int kol)
 
     if (Board[wier][kol].isRevealed == false and Board[wier][kol].hasMine == true)
     {
-        Board[wier][kol].isRevealed = true;
-        FINISHED_LOSS;
+        
+        for (int i = 0; i < wier; i++)
+        {
+            for (int j = 0; j < kol; j++)
+            {
+                if (Board[i][j].isRevealed == true)
+                {
+                    notTouchedBoard = false;
+                }
+            }
+        }
+
+        if (notTouchedBoard == true)
+        {
+            Board[wier][kol].isRevealed = true;
+            Board[wier][kol].hasMine = false;
+            Board[rand() % wier - 1][rand() % kol - 1].hasMine = true;
+        }
+
+        else
+        {
+            Board[wier][kol].isRevealed = true;
+            FINISHED_LOSS;
+        }
+
     }
 
 }
 
-bool MinesweeperBoard::isRevealed(int wier, int kol) const
-{
-    if (Board[wier][kol].isRevealed == true)
-        return true;
-}
 
 char MinesweeperBoard::getFieldInfo(int wier, int kol) const
 {
@@ -181,61 +275,14 @@ char MinesweeperBoard::getFieldInfo(int wier, int kol) const
         return countMines(wier, kol);
 }
 
-/*
-
-MinesweeperBoard::MinesweeperBoard(int width, int height, GameMode mode)
+GameState MinesweeperBoard::getGameState() const
 {
-  //width = 10;
-  //height = 10; 
-  
-  for (int wier = 0; wier < height; wier++)
-  {
-    for (int kol = 0; kol < width; kol++)
-    {
-      Board [wier][kol].hasMine = false;
-      Board [wier][kol].isRevealed = false;
-      Board [wier][kol].hasFlag = false;
-    }
-  }
+    if (FINISHED_LOSS)
+        return FINISHED_LOSS;
 
+    if (FINISHED_WIN)
+        return FINISHED_WIN;
 
-  Board [0][0].hasMine = true;
-  Board [0][0].hasFlag = false;
-  Board [0][0].isRevealed = false;
-
-  Board [1][1].hasMine = false;
-  Board [1][1].hasFlag = false;
-  Board [1][1].isRevealed = true;
-
-  Board [0][2].hasMine = true;
-  Board [0][2].hasFlag = true;
-  Board [0][2].isRevealed = false;
-
+    if (RUNNING)
+        return RUNNING;
 }
-
-void MinesweeperBoard::debug_display() const
-{
-  for (int wier = 0; wier < height; wier++)
-  {
-    for (int kol = 0; kol < width; kol++)
-    {
-      if (Board [wier][kol].hasMine == true)
-        std::cout << "[M";
-      else
-        std::cout << "[.";
-
-      if (Board [wier][kol].isRevealed == true)
-        std::cout << "o";
-      else
-        std::cout << ".";
-
-      if (Board [wier][kol].hasFlag == true)
-        std::cout << "f]";
-      else
-        std::cout << ".]"; 
-    }
-    std::cout << std::endl;
-  }
-}
-
-*/
